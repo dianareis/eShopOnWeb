21fca8d3cbd559dd04a9e5df2edd8e3ca94cbd4b
dianareis <dianamrreis@gmail.com>
2020-02-20 23:44:45 +0000

    Authentication and WishListTest

--- src/Infrastructure/Identity/ApplicationUser.cs
+++ src/Infrastructure/Identity/ApplicationUser.cs
@@ -3,5 +3,8 @@
 namespace Microsoft.eShopWeb.Infrastructure.Identity
 {
     public class ApplicationUser : IdentityUser
-    {}
+    {
+        public string Name { get; set; }
+        public string ProviderName { get; set; }
+    }
 }
